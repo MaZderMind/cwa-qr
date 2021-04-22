d1af9a8489c437c20085b7b041d0714af4e37f52
Peter Körner <pkoerner@seibert-media.net>
2021-04-22 21:41:40 +0200

    add pytest tests

---
+++ cwa/test_cwa.py
@@ -0,0 +1,64 @@
+import io
+import secrets
+from datetime import datetime, timedelta, timezone
+
+from . import cwa
+
+fullEventDescription = cwa.CwaEventDescription()
+fullEventDescription.locationDescription = 'Zuhause'
+fullEventDescription.locationAddress = 'Gau-Odernheim'
+fullEventDescription.startDateTime = datetime.now(timezone.utc)
+fullEventDescription.endDateTime = datetime.now(timezone.utc)
+fullEventDescription.locationType = cwa.lowlevel.LOCATION_TYPE_PERMANENT_WORKPLACE
+fullEventDescription.defaultCheckInLengthInMinutes = 4 * 60
+
+def test_generate_with_minimal_parameters():
+	minimalEventDescription = cwa.CwaEventDescription()
+	minimalEventDescription.locationDescription = 'Zuhause'
+	minimalEventDescription.locationAddress = 'Gau-Odernheim'
+	url = cwa.generateUrl(minimalEventDescription)
+	assert url != ''
+
+def test_generate_with_all_parameters():
+	url = cwa.generateUrl(fullEventDescription)
+	assert url != ''
+
+def test_generate_without_given_seed_creates_different_results():
+	urlA = cwa.generateUrl(fullEventDescription)
+	urlB = cwa.generateUrl(fullEventDescription)
+	assert urlA != urlB
+
+def test_generate_wit_given_seed_creates_same_result():
+	fullEventDescription.randomSeed = secrets.token_bytes(16)
+	urlA = cwa.generateUrl(fullEventDescription)
+	urlB = cwa.generateUrl(fullEventDescription)
+	assert urlA == urlB
+
+
+def test_generate_payload_object():
+	payload = cwa.generatePayload(fullEventDescription)
+	assert payload is not None
+
+def test_generate_qr_code():
+	qr = cwa.generateQrCode(fullEventDescription)
+	assert qr is not None
+
+def test_generate_qr_code_png():
+	qr = cwa.generateQrCode(fullEventDescription)
+	img = qr.make_image(fill_color="black", back_color="white")
+
+	img_bytes = io.BytesIO()
+	img.save(img_bytes)
+
+	assert img_bytes.getvalue().startswith(b'\x89PNG\r\n\x1a\n')
+
+def test_generate_qr_code_svg():
+	import qrcode.image.svg
+
+	qr = cwa.generateQrCode(fullEventDescription)
+	svg = qr.make_image(image_factory=qrcode.image.svg.SvgPathFillImage)
+
+	svg_bytes = io.BytesIO()
+	svg.save(svg_bytes)
+
+	assert svg_bytes.getvalue().startswith(b'<?xml')
