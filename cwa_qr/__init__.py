c909bd85d5d84b6502b34a6c33b33b4ebde66b89
MaZderMind <git@mazdermind.de>
2021-05-03 21:09:45 +0200

    move poster code into its own file

--- cwa_qr/__init__.py
+++ cwa_qr/__init__.py
@@ -1,5 +1,6 @@
-from .cwa import (CwaEventDescription, CwaLocation, CwaPoster, generate_payload,
-                  generate_poster, generate_qr_code, generate_url, lowlevel)
+from .cwa import (CwaEventDescription, CwaLocation, generate_payload,
+                  generate_qr_code, generate_url, lowlevel)
+from .poster import CwaPoster, generate_poster
 
 from .rollover import rollover_date
 
---
+++ cwa_qr/test_poster.py
@@ -0,0 +1,19 @@
+from . import cwa, poster
+
+event_description = cwa.CwaEventDescription()
+event_description.location_description = 'Zuhause'
+event_description.location_address = 'Gau-Odernheim'
+
+
+def test_generate_qr_code_with_poster_portrait_svg():
+    poster_svg = poster.generate_poster(event_description, poster.CwaPoster.POSTER_PORTRAIT)
+
+    svg_str = poster_svg.to_str()
+    assert svg_str.startswith(b'<?xml')
+
+
+def test_generate_qr_code_with_poster_landscape_svg():
+    poster_svg = poster.generate_poster(event_description, poster.CwaPoster.POSTER_LANDSCAPE)
+
+    svg_str = poster_svg.to_str()
+    assert svg_str.startswith(b'<?xml')
